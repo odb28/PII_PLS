7c612275e7dc61b42b138d522d66f6318736f633
odb28 <odb28@cam.ac.uk>
2023-11-02 11:55:57 +0000

    02/11/23

--- Scripts/SIR Collected Means.py
+++ Scripts/SIR Collected Means.py
@@ -1,4 +1,5 @@
 import numpy as np
+import arviz as az
 
 N_10_b = np.empty(shape = (10,2))
 N_100_b = np.empty(shape = (10,2))
@@ -6,40 +7,37 @@ N_1000_b = np.empty(shape = (10,2))
 N_10_g = np.empty(shape = (10,2))
 N_100_g = np.empty(shape = (10,2))
 N_1000_g = np.empty(shape = (10,2))
-
+#%%
 for i in range(1,11):
-    b_10 = np.genfromtxt(f"../Data/Traj_{i}_N_10_b.csv", delimiter= ",")
-    b_100 = np.genfromtxt(f"../Data/Traj_{i}_N_100_b.csv", delimiter= ",")
-    b_1000 = np.genfromtxt(f"../Data/Traj_{i}_N_1000_b.csv", delimiter= ",")
-    g_10 = np.genfromtxt(f"../Data/Traj_{i}_N_10_g.csv", delimiter= ",")
-    g_100 = np.genfromtxt(f"../Data/Traj_{i}_N_100_g.csv", delimiter= ",")
-    g_1000 = np.genfromtxt(f"../Data/Traj_{i}_N_1000_g.csv", delimiter= ",")
+    b_10 = np.ndarray.flatten(np.genfromtxt(f"../Data/Traj_{i}_N_10_b.csv", delimiter= ","))
+    b_100 = np.ndarray.flatten(np.genfromtxt(f"../Data/Traj_{i}_N_100_b.csv", delimiter= ","))
+    b_1000 = np.ndarray.flatten(np.genfromtxt(f"../Data/Traj_{i}_N_1000_b.csv", delimiter= ","))
+    g_10 = np.ndarray.flatten(np.genfromtxt(f"../Data/Traj_{i}_N_10_g.csv", delimiter= ","))
+    g_100 = np.ndarray.flatten(np.genfromtxt(f"../Data/Traj_{i}_N_100_g.csv", delimiter= ","))
+    g_1000 = np.ndarray.flatten(np.genfromtxt(f"../Data/Traj_{i}_N_1000_g.csv", delimiter= ","))
 
-    N_10_b[i-1][0] = np.mean(b_10)
+
+    N_10_b[i-1][0] = az.kde(b_10)[0][np.argmax(az.kde(b_10)[1])]
     N_10_b[i-1][1] = np.std(b_10)
 
-    N_100_b[i-1][0] = np.mean(b_100)
+    N_100_b[i-1][0] = az.kde(b_100)[0][np.argmax(az.kde(b_100)[1])]
     N_100_b[i-1][1] = np.std(b_100)
 
-    N_1000_b[i-1][0] = np.mean(b_1000)
+    N_1000_b[i-1][0] = az.kde(b_1000)[0][np.argmax(az.kde(b_1000)[1])]
     N_1000_b[i-1][1] = np.std(b_1000)
 
-    N_10_g[i-1][0] = np.mean(g_10)
+    N_10_g[i-1][0] = az.kde(g_10)[0][np.argmax(az.kde(g_10)[1])]
     N_10_g[i-1][1] = np.std(g_10)
 
-    N_100_g[i-1][0] = np.mean(g_100)
+    N_100_g[i-1][0] = az.kde(g_100)[0][np.argmax(az.kde(g_100)[1])]
     N_100_g[i-1][1] = np.std(g_100)
 
-    N_1000_g[i-1][0] = np.mean(g_1000)
+    N_1000_g[i-1][0] = az.kde(g_1000)[0][np.argmax(az.kde(g_1000)[1])]
     N_1000_g[i-1][1] = np.std(g_1000)
 
-Ns =[10, 100, 1000]
-paras = ["b","g"]
-
-np.savetxt("../Data/N_10_b",N_10_b,delimiter= ",")
-np.savetxt("../Data/N_100_b",N_100_b,delimiter= ",")
-np.savetxt("../Data/N_1000_b",N_1000_b,delimiter= ",")
-
-np.savetxt("../Data/N_10_g",N_10_g,delimiter= ",")
-np.savetxt("../Data/N_100_g",N_100_g,delimiter= ",")
-np.savetxt("../Data/N_1000_g",N_1000_g,delimiter= ",")
+np.savetxt("../Data/N_1000_b.csv",N_1000_b,delimiter=",")
+np.savetxt("../Data/N_100_b.csv",N_100_b,delimiter=",")
+np.savetxt("../Data/N_10_b.csv",N_10_b,delimiter=",")
+np.savetxt("../Data/N_1000_g.csv",N_1000_g,delimiter=",")
+np.savetxt("../Data/N_100_g.csv",N_100_g,delimiter=",")
+np.savetxt("../Data/N_10_g.csv",N_10_g,delimiter=",")
---
+++ Scripts/SIR MLE.py
@@ -0,0 +1,41 @@
+import numpy as np
+import scipy as sp
+from src.baseSIR import mle
+from src.baseSIR import mle_sir
+import time
+def opt_mle(paras):
+    return - mle(paras[0],paras[1],timed_real,times_real,tmax)
+
+mle_b = []
+mle_g = []
+start_time = time.time()
+for i in range(1,11):
+    X0 = [900,100,0]
+    beta = 3
+    gamma = 1
+    mu = 0
+    tmax = 10
+    tstep = 0.05
+
+    rng = np.random.default_rng(i)
+    start_time = time.time()
+    reality,times_real, timed_real = mle_sir(X0, mu, beta, gamma, tmax,tstep,rng=rng)
+    #print(f"Reality took {time.time() - start_time} seconds to run!")
+    times = np.arange(0,tmax,tstep)
+    found = False
+    for j in range(10):
+        w = sp.optimize.minimize(opt_mle,x0=(2.5+j/10,0.5+j/10))
+        if found == False:
+            found = True
+            best_b = w.x[0]
+            best_g = w.x[1]
+            best = w.fun
+        elif w.fun > best:
+            best_b = w.x[0]
+            best_g = w.x[1]
+    mle_b.append(best_b)
+    mle_g.append(best_g)
+print(f"MLE took {time.time() - start_time} seconds to run")
+
+np.savetxt(f"../Data/MLE_b.csv", mle_b, delimiter=",")
+np.savetxt(f"../Data/MLE_g.csv", mle_g, delimiter=",")
