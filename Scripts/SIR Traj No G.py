88369d6dddf7b1e9a25143099f2c0896808bf02a
odb28 <odb28@cam.ac.uk>
2023-11-02 14:38:51 +0000

    02/11/23 2

--- Scripts/SIR Traj No G.py
+++ Scripts/SIR Traj No G.py
@@ -8,7 +8,7 @@ if __name__ == '__main__':
 
 
     #Initialise RNG
-    seed = 9
+    seed = 10
     rng = np.random.default_rng(seed)
     def sim_sir(model_rng, b, size=None):
         return real_sir(X0, mu, b, gamma, tmax, tstep, rng) * factor
